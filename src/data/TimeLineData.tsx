07dac9eafaf190f2e59af20e74f34b66264af70e
Sabeshragav <sabeshragav289@gmail.com>
2025-03-21 10:57:33 +0530

    new paper-present image

--- src/data/TimeLineData.tsx
+++ src/data/TimeLineData.tsx
@@ -8,7 +8,7 @@ export const timeLineData = [
         <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
           Built and launched Aceternity UI and Aceternity UI Pro from scratch
         </p>
-        <div className="grid grid-cols-2 gap-4">
+        {/* <div className="grid grid-cols-2 gap-4">
           <Image
             src="/events/tech/paper-present.png"
             alt="startup template"
@@ -37,7 +37,7 @@ export const timeLineData = [
             height={500}
             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
           />
-        </div>
+        </div> */}
       </div>
     ),
   },
@@ -53,7 +53,7 @@ export const timeLineData = [
           Lorem ipsum is for people who are too lazy to write copy. But we are
           not. Here are some more example of beautiful designs I built.
         </p>
-        <div className="grid grid-cols-2 gap-4">
+        {/* <div className="grid grid-cols-2 gap-4">
           <Image
             src="/events/tech/paper-present.png"
             alt="hero template"
@@ -82,7 +82,7 @@ export const timeLineData = [
             height={500}
             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
           />
-        </div>
+        </div> */}
       </div>
     ),
   },
@@ -110,7 +110,7 @@ export const timeLineData = [
             ✅ Salman Bhai Fan Club registrations open
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-4">
+        {/* <div className="grid grid-cols-2 gap-4">
           <Image
             src="/events/tech/paper-present.png"
             alt="hero template"
@@ -139,7 +139,7 @@ export const timeLineData = [
             height={500}
             className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
           />
-        </div>
+        </div> */}
       </div>
     ),
   },
